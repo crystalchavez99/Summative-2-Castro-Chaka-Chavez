e98b17bbfe8ea7d96a2d7343eff257674327f14c
‘Gelila’ <79760415+Gelila21@users.noreply.github.com>
2023-08-04 02:13:53 +0300

    publisher classes

--- src/main/java/com/company/bookstore/controller/GraphController.java
+++ src/main/java/com/company/bookstore/controller/GraphController.java
@@ -8,6 +8,9 @@ import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
+import com.company.bookstore.models.Publisher;
+import com.company.bookstore.repository.PublisherRepository;
+
 import java.time.LocalDate;
 import java.util.List;
 
@@ -16,6 +19,10 @@ public class GraphController {
 
     @Autowired
     BookRepository bookRepository;
+
+    @Autowired
+    PublisherRepository publisherRepository;
+
 
     @QueryMapping
     public List<Book> books(){
@@ -58,4 +65,69 @@ public class GraphController {
     public boolean deleteBookById(@Argument String id){
         return bookRepository.deleteBookById(id);
     }
+
+
+//PublisherController
+
+    @QueryMapping
+    public List<Publisher> publishers() {
+        return publisherRepository.getAllPublishers();
+    }
+
+    @QueryMapping
+    public Publisher findPublisherById(@Argument String id) {
+        return publisherRepository.getPublisherById(id);
+    }
+
+    @MutationMapping
+    public Publisher addPublisher(
+            @Argument String name,
+            @Argument String street,
+            @Argument String city,
+            @Argument String state,
+            @Argument String postalCode,
+            @Argument String phone,
+            @Argument String email
+    ) {
+        Publisher newPublisher = new Publisher();
+        newPublisher.setName(name);
+        newPublisher.setStreet(street);
+        newPublisher.setCity(city);
+        newPublisher.setState(state);
+        newPublisher.setPostalCode(postalCode);
+        newPublisher.setPhone(phone);
+        newPublisher.setEmail(email);
+        return publisherRepository.addPublisher(newPublisher);
+    }
+
+    @MutationMapping
+    public Publisher updatePublisher(
+            @Argument String name,
+            @Argument String street,
+            @Argument String city,
+            @Argument String state,
+            @Argument String postalCode,
+            @Argument String phone,
+            @Argument String email
+    ) {
+        Publisher updatePublisher = new Publisher();
+        updatePublisher.setName(name);
+        updatePublisher.setStreet(street);
+        updatePublisher.setCity(city);
+        updatePublisher.setState(state);
+        updatePublisher.setPostalCode(postalCode);
+        updatePublisher.setPhone(phone);
+        updatePublisher.setEmail(email);
+        return publisherRepository.updatePublisher(updatePublisher);
+    }
+
+    @MutationMapping
+    public boolean deletePublisherById(@Argument String id) {
+        return publisherRepository.deletePublisherById(id);
+    }
+
+
 }
+
+
+
